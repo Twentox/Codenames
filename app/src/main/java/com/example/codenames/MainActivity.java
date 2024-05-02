e31db94cc916b51e844fcc72dd4abb135f1f2f83
Erik <114912056+Twentox@users.noreply.github.com>
2024-05-02 12:12:15 +0200

    added onClick Method
    for menu

--- app/src/main/java/com/example/codenames/MainActivity.java
+++ app/src/main/java/com/example/codenames/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.codenames;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +12,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button create_game_btn;
+    private Button join_game_btn;
+    private Button game_rules_btn;
+    private View main_layout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.menu_activity);
-       // setContentView(R.layout.game_activity);
+
+        create_game_btn = findViewById(R.id.create_game_btn);
+        join_game_btn = findViewById(R.id.join_game_btn);
+        game_rules_btn = findViewById(R.id.game_rules_btn);
+        main_layout = findViewById(R.id.main);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -23,4 +36,20 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void createGame(View view){
+        main_layout.setVisibility(view.GONE);
+        setContentView(R.layout.invite_players_activity);
+
+    }
+    public void joinGame(View view){
+        main_layout.setVisibility(view.GONE);
+        setContentView(R.layout.join_game_activity);
+    }public void gameRules(View view){
+        main_layout.setVisibility(view.GONE);
+        setContentView(R.layout.game_rules_activity);
+    }
+
+
+
 }
