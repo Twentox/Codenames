277d12e3442a433c7d58f742b7628fc2c5458c86
Erik <114912056+Twentox@users.noreply.github.com>
2024-05-02 11:35:38 +0200

    added branch appHochkant
    added branch appHochkant

--- app/src/main/java/com/example/codenames/MainActivity.java
+++ app/src/main/java/com/example/codenames/MainActivity.java
@@ -14,10 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-       // setContentView(R.layout.menu_activity);
-        setContentView(R.layout.game_activity);
-        System.out.println("asdasdasd");
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.game), (v, insets) -> {
+        setContentView(R.layout.menu_activity);
+       // setContentView(R.layout.game_activity);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
