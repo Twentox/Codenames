2977cce72b0a51f84542fa1290bf256f3a095f2b
Finn Kruse <finn.henrik.kruse@gmail.com>
2024-10-07 17:08:18 +0200

    nutten

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.codenames"
-        minSdk = 24
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -29,12 +29,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
 }
--- app/src/androidTest/java/com/example/codenames/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/codenames/ExampleInstrumentedTest.java
@@ -1,14 +1,18 @@
 package com.example.codenames;
 
-import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import static org.junit.Assert.*;
+import com.example.codenames.Activities.MenuActivity;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +21,39 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.codenames", appContext.getPackageName());
+
+
+    @RunWith(AndroidJUnit4.class)
+    public class CreateGameViewTest {
+
+        @Rule
+        public ActivityScenarioRule<MenuActivity> activityRule =
+                new ActivityScenarioRule<>(MenuActivity.class);
+
+        @Test
+        public void testButtonClickOpensNewActivity() {
+            onView(withId(R.id.create_game_btn)).perform(click());
+            onView(withId(R.id.invite_players)).check(matches(withText("invite_players geladen")));
+        }
     }
+
+
+    @RunWith(AndroidJUnit4.class)
+    public class gameRulesViewTest {
+
+        @Rule
+        public ActivityScenarioRule<MenuActivity> activityRule =
+                new ActivityScenarioRule<>(MenuActivity.class);
+
+        @Test
+        public void testButtonClickOpensNewActivity() {
+            onView(withId(R.id.game_rules_btn)).perform(click());
+            onView(withId(R.id.game_rules)).check(matches(withText("game_rules geladenD")));
+        }
+    }
+
+
+
+
+
 }
